6ad910b604468ce8ff281fab1b61ff3b2ae3b669
dieggspapu <alo20172@uvg.edu.gt>
2023-06-02 15:22:34 -0600

    Just the chart left

--- src/components/gridevice/gridevice.jsx
+++ src/components/gridevice/gridevice.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './gridevice.css'
+import styles from './gridevice.module.css'
 
 export default function gridevice({ data }) {
   return (
-    <div className="container-grid">
+    <div className={styles.container}>
       {
                 data.map((column) => (
-                  <div className="container-column">
+                  <div className={styles.containerColumn}>
                     <img src={column[0]} alt="Device" />
                     <h2>{column[1]}</h2>
-                    <div className="data-section">
+                    <div className={styles.dataSection}>
                       {
                             column[2].map((item) => (<span>{item}</span>))
                         }
--- src/components/section6/section6.jsx
+++ src/components/section6/section6.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Devices from '../gridevice/gridevice'
-import './section6.css'
+import styles from './section6.module.css'
 import Computer from '../../assets/computer.png'
 import tv from '../../assets/tv.png'
 import videogames from '../../assets/videogames.png'
@@ -8,7 +8,7 @@ import tablet from '../../assets/tablet.png'
 
 export default function section6() {
   return (
-    <div className="container-6">
+    <div className={styles.container}>
       <h2>Available on your favorite devices</h2>
       <Devices data={[[Computer, 'Computer', ['Chrome OS', 'MacOS', 'Windows PC']],
         [tv, 'TV', ['Amazon Fire TV', 'Android TV', 'Apple TV', 'Chromecast', 'LG TV\'s', 'Roku', 'Samsung']],
